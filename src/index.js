44da6a0b83558c22d75a7abc1733b7d1175cc6a4
Arpad TOTH <arpad.toth@zeiss.com>
2022-12-16 20:08:21 +0100

    minor adjustments, cors added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const baseUrl = "http://localhost:6060";
 
-// API INTERACTIONS
-const fetchFileList = (url) => {
+/* ............................ API INTERACTIONS ............................ */
+const fetchFileList = async (url) => {
   let promise = axios
     .get(url + "/file", { responseType: "text" })
     .then((response) => {
@@ -12,7 +12,7 @@ const fetchFileList = (url) => {
 };
 
 // TODO: Ezt a main function hívja meg! Kiszervezni!
-const fetchFileContent = (url, id) => {
+const fetchFileContent = async (url, id) => {
   let promise = axios
     .get(url + "/file/" + id, { responseType: "text" })
     .then((response) => {
@@ -30,7 +30,17 @@ const uploadFile = async (url, formData) => {
   })
 };
 
-// INDEX PAGE INTERACTIONS
+const deleteFile = async (url, id) => {
+  let promise = axios
+    .delete(url + "/file/" + id, { responseType: "text" })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => console.log(err));
+  return promise;
+};
+
+/* ......................... INDEX PAGE INTERACTIONS ......................... */
 const createUploadButton = (parentId) => {
   const uploadField = document.getElementById(parentId);
   const inputButton = document.createElement("input");
@@ -66,16 +76,31 @@ const createNewTableRow = (tableId, filename) => {
       cell.innerHTML += filename;
     } else if (i == 4) {
       cell.className = "tg-cell actions";
-      const button = cell.appendChild(document.createElement("input"));
-      button.type = "button";
-      button.class = "submit-button";
-      button.value = "Get content";
-      button.id = `get-file-content-submit-${filename}`;
+      
+      const button_view = cell.appendChild(document.createElement("input"));
+      button_view.type = "button";
+      button_view.class = "submit-button";
+      button_view.value = "View";
+      button_view.id = `get-file-content-submit-${filename}`;
+      
       document.getElementById(`get-file-content-submit-${filename}`).onclick =
         async () => {
           let content = await fetchFileContent(baseUrl, filename);
           createTextBox(content);
         };
+
+      const button_delete = cell.appendChild(document.createElement("input"));
+      button_delete.type = "button";
+      button_delete.class = "submit-button";
+      button_delete.value = "Delete";
+      button_delete.id = `delete-file-submit-${filename}`;
+      
+      document.getElementById(`delete-file-submit-${filename}`).onclick =
+        async () => {
+          if (confirm("Are you sure?") == true) {
+            await deleteFile(baseUrl, filename);
+          }
+        };  
     } else {
       cell.className = "tg-cell";
       cell.innerHTML += "data";
@@ -83,15 +108,24 @@ const createNewTableRow = (tableId, filename) => {
   }
 };
 
-//MAIN
-const createFileListTable = async () => {
-  // be kell burkolni az await-et egy asyncbe, mert csak azon belül működik
-  let promise = fetchFileList(baseUrl); // ez kb egy task v todo, "ezt kell csinálni"
-  let fileList = await promise; // az await indítja a munkát és meg is várja az eredményt, az async kód sync lesz
+/* .............................. MAIN .............................. */
+const main = async () => {                // be kell burkolni az await-et egy asyncbe, mert csak azon belül működik
+  let promise = fetchFileList(baseUrl);   // ez kb egy task v todo, "ezt kell csinálni"
+  let fileList = await promise;           // az await indítja a munkát és meg is várja az eredményt, az async kód sync lesz
   let fileListArray = fileList.match(/[[a-zA-Z0-9]+\.srt*/gm);
 
-  for (let i = 0; i < fileListArray.length; i++) {
-    createNewTableRow("files-table", fileListArray[i]);
+  if (fileListArray != null) {
+    for (let i = 0; i < fileListArray.length; i++) {
+      createNewTableRow("files-table", fileListArray[i]);
+    }
+  } else {
+    let table = document.getElementById("files-table");
+    let body = table.getElementsByTagName("tbody")[0];
+    let row = body.insertRow(-1);
+    let cell = row.insertCell(0);
+    cell.colSpan = table.rows[0].cells.length;
+    cell.innerText = "No data!";
+    cell.style.textAlign = "center";
   }
 
   const uploadField = document.getElementById("file-upload-field");
@@ -99,20 +133,22 @@ const createFileListTable = async () => {
     if (document.getElementById("file-submit")) {
       document.getElementById("file-submit").remove();
     }
-    
-    let form = new FormData
-    form.append('file', uploadField.files[0], 'filename');
-    form.append('title', 'this is title');
-    form.append('imdbscore', 5.5);
-    // for (const pair of form.entries()) {console.log(`${pair[0]}, ${pair[1]}`);}
+
+    let form = new FormData();
+    form.append("file", uploadField.files[0], "filename");
+    form.append("title", "this is title");
+    form.append("imdbscore", 5.5);
+    for (const pair of form.entries()) {
+      console.log(`${pair[0]}, ${pair[1]}`);
+    }
 
     createUploadButton("file-upload-field");
     let uploadButton = document.getElementById("file-submit");
 
     uploadButton.onclick = () => {
-      uploadFile(baseUrl, form)
-    }
+      uploadFile(baseUrl, form);
+    };
   });
 };
 
-createFileListTable();
+main();
